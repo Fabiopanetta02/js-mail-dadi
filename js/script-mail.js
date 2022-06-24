edcc9a52e62c601bb9f2f0a48d25a7d1858c176b
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-06-24 15:37:01 +0200

    ADDED ex dadi e email

--- js/script-mail.js
+++ js/script-mail.js
@@ -1,25 +1,31 @@
 /*Mail
-1--Crea (inventa) una lista di inidirizzi mail autorizzati.
+1--Crea (inventa) una lista di indirizzi mail autorizzati.
 2--Chiedi all’utente la sua email,
 3--controlla che sia nella lista di chi può accedere,
 4--stampa in console un messaggio appropriato sull’esito del controllo. 
 ("Benvenuto" se la mail è nella lista, "Spiacenti" se non lo è);
 */
 
-//1--Crea (inventa) una lista di inidirizzi mail autorizzati.
+//1--Crea (inventa) una lista di indirizzi mail autorizzati.
 const emailCorrect = ["pinco@gmail.com", "pallino@gmail.com", "cip@gmail.com", "cop@gmail.com"];
 console.log(emailCorrect);
 
 //2--Chiedi all’utente la sua email,
-let emailUser = prompt("Qual'è la tua email?", "pinco@gmail.com");
+const emailUser = prompt("Qual'è la tua email?", "pinco@gmail.com");
 console.log(emailUser);
 
 //3--controlla che sia nella lista di chi può accedere
+let fleg = false
 for (let i = 0; i < emailCorrect.length; i++){
     if(emailUser == emailCorrect[i]){
-        console.log('Benvenuto');
-    }
-    else{
-        console.log('Spiacenti')
+        fleg = true
     }
 }
+
+if(fleg == true){
+    console.log('Benvenuto')
+}
+else{
+    console.log('Spiacenti')
+}
+
